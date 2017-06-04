fb02529d695173128af48c3e9fb3998f2c0204ef
Luchanso <luchansodev@gmail.com>
2017-06-04 16:28:06 +0300

    view: finish WIP page

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WorkInProgress from '../WorkInProgress';
+import css from './App.css';
+
+const App = () => (
+  <div className={css.App}>
+    <WorkInProgress />
+  </div>
+);
+
+export default App;
